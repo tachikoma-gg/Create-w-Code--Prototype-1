25ff78516e9ceaff8b187b29328d9b1379e1f4a1
Silvia Võ <40185806+tachikoma-gg@users.noreply.github.com>
2024-10-10 15:28:25 -0700

    camera follow player

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
     }
 }
